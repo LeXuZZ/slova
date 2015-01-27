c563505017bdc644dc0002fa8244d6a6b08c727a
LeXuZZ <oleksiy.chornenkyy@gmail.com>
2015-01-27 21:30:27 +0200

    small fixes

--- algorithms.py
+++ algorithms.py
@@ -1,20 +1,13 @@
 # coding=utf-8
 from random import choice, shuffle, sample
 from addict import Dict
-
 __author__ = 'lxz'
-# empty_grid = [[0, 0, 0],
-#               [0, 0, 0],
-#               [0, 0, 0]]
-#
-# grid = [[0, 0, 0],
-#         [0, 0, 0],
-#         [0, 0, 0]]
-
 
 class STATIC():
+    WORDS = ['HELLO', 'BUNNY', 'ADDICT']
     OUT_OF_GRID = 'OUT_OF_GRID'
     EMPTY_CELL_LETTER = '0'
+    GAME_DIMENSIONS = 5
 
 
 def check_grid_for_ability_to_enter_3letter_words(grid):
@@ -36,7 +29,7 @@ def add_word_to_grid():
 
 
 class smart_cell():
-    def __init__(self, row, cell, game_dimensions):
+    def __init__(self, row, cell, game_dimensions=STATIC.GAME_DIMENSIONS):
         self.id = '{0}-{1}'.format(row, cell)
         self.letter = STATIC.EMPTY_CELL_LETTER
         self.flag = None
@@ -44,14 +37,15 @@ class smart_cell():
         self.down = '{0}-{1}'.format(row + 1, cell) if row != game_dimensions - 1 else None
         self.left = '{0}-{1}'.format(row, cell - 1) if cell != 0 else None
         self.right = '{0}-{1}'.format(row, cell + 1) if cell != game_dimensions - 1 else None
+        pass
 
 
-def create_smart_grid(game_dimensions=5):
+def create_smart_grid(game_dimensions=STATIC.GAME_DIMENSIONS):
     smart_grid = Dict()
     for dimension in xrange(game_dimensions):
         smart_grid[dimension] = []
     for row in smart_grid:
-        smart_grid[row] = [smart_cell(row, cell, game_dimensions) for cell in xrange(game_dimensions)]
+        smart_grid[row] = [smart_cell(row, cell) for cell in xrange(game_dimensions)]
     return smart_grid
 
 
@@ -89,7 +83,6 @@ def _delete_move_ability_to(cell):
 
 def fill_a_word_to_grid(word, start_cell_id, grid):
     splitted_word = list(word)
-    word_length = len(splitted_word)
     start_cell = search_cell_in_grid(start_cell_id, grid)
     start_cell.letter = splitted_word[0]
     previous_cell = start_cell
@@ -133,7 +126,7 @@ def run_algo():
 if __name__ == "__main__":
     grid = create_smart_grid()
     needed_cell = search_cell_in_grid('0-4', grid)
-    new_grid = fill_a_word_to_grid('123456789qwerty', '2-3', grid)
+    new_grid = fill_a_word_to_grid(STATIC.WORDS[0], '2-1', grid)
     print_grid_to_console(new_grid)
     print check_grid_for_ability_to_enter_3letter_words(new_grid)
     pass
