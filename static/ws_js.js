6a8fb06cf323ffd03f0d01c64219d2d5406003c2
LeXuZZ <oleksiy.chornenkyy@gmail.com>
2015-02-23 21:19:51 +0200

    yakes layno

--- static/ws_js.js
+++ static/ws_js.js
@@ -24,7 +24,7 @@ $(function () {
             $rowCache[i] = $('#row' + i);
 
             for (var j = 0; j < gameDimensions; j++) {
-                $rowCache[i].append('<div id="' + j + '-' + i + '" class="cell" style="top:' + i * cellSize + 'px; left:' + j * cellSize + 'px;" ></div>');
+                $rowCache[i].append('<div id="' + i + '-' + j + '" class="cell noselect" style="top:' + i * cellSize + 'px; left:' + j * cellSize + 'px;" ></div>');
             }
         }
 
@@ -46,13 +46,13 @@ $(function () {
         var gd = 8;
         var wc = 10;
         var colorList = [];
-        for (var c = 0; c < wc; c ++) {
+        for (var c = 0; c < wc; c++) {
             colorList.push(getRandomColor());
         }
-        for (var i = 0; i < gd; i ++) {
+        for (var i = 0; i < gd; i++) {
             var color = getRandomColor();
             var row = grid[i];
-            for (var j = 0; j < gd; j ++) {
+            for (var j = 0; j < gd; j++) {
                 $('div#' + i + '-' + j + '.cell').append('<span>' + row[j].letter.value + '</span>').css('background-color', colorList[row[j].letter.value]);
                 //$('div#' + i + '-' + j + '.cell span').css('color', colorList[row[j].letter.value]);
             }
@@ -60,15 +60,57 @@ $(function () {
 
     };
 
-    var addLetterToCell = function (msg) {
-        $('div#0-0.cell').append('<span>' + msg["0"]["0"].letter.value + '</span>')
-    };
-
     ws.onmessage = function (msg) {
         //console.log('message is: ' + msg.data);
         var message = $.parseJSON(msg.data);
-        //addLetterToCell(message);
         fillGrid(message)
     };
 
+    //$('div.cell').on("mouseenter mouseleave", function (e) {
+    //    $(this).toggleClass('selected', e.type == 'mouseenter');
+    //});
+
+
+    //var elem_list = [];
+    //$('div.cell').on('mousedown', function (e) {
+    //    $(this).addClass('selected');
+    //    $('div.cell').one('mouseenter.c', function (e) {
+    //        $(this).toggleClass('selected', e.type == 'mouseenter.c');
+    //        elem_list.push(this)
+    //    });
+    //}).on('mouseup', function (e) {
+    //    elem_list.forEach(function (elem) {
+    //        $(elem).toggleClass('selected')
+    //    });
+    //    $('div.cell').on('mouseenter.c');
+    //    elem_list = []
+    //});
+    var if_elem_nearby = function (id1, id2) {
+        var id1 = [id1[0], id1[2]];
+        var id2 = [id2[0], id2[2]];
+    };
+
+    var tmp_elem;
+    var elem_list = [];
+    var handler = function () {
+        if (!tmp_elem.isNaN() && if_elem_nearby(this, tmp_elem)) {
+            $(this).addClass('selected');
+            elem_list.push(this);
+            tmp_elem = this;
+            console.log($(tmp_elem).attr('id'))
+        }
+    };
+    $('div.cell').mousedown(function () {
+        $(this).addClass('selected');
+        elem_list.push(this);
+        tmp_elem = this;
+        console.log($(tmp_elem).attr('id'));
+        $('div.cell').on('mouseenter.с', handler)
+    }).mouseup(function () {
+        elem_list.forEach(function (elem) {
+            //console.log(elem);
+            $(elem).removeClass('selected')
+        });
+        $('div.cell').off('mouseenter.с', handler)
+    })
 });
